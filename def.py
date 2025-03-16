e10d1e4debab31f26bddefa2871f4dd174ac2d5a
kidusshoa <captainkevin2008@gmail.com>
2025-03-16 23:59:54 +0300

    python function

---
+++ def.py
@@ -0,0 +1,17 @@
+# def greet(name="Leul"):
+#     return f"Hello, {name}!!!"
+
+# print(greet())
+# print(greet("Abenezer"))
+
+# def sum(x, y = 30, z = 20):
+#     final = x + y + z
+#     return final
+
+# print(sum(10, 20, 13))
+
+# def print_nums(n):
+#     for num in range(1, n + 1):
+#         print(num, end="  ")
+
+# print_nums(10)
