79a205fdc9a6fc5c2100f304139ff32a59c08d50
kidusshoa <captainkevin2008@gmail.com>
2025-03-16 23:25:02 +0300

    one syntax data types

---
+++ one.py
@@ -0,0 +1,17 @@
+
+class_attendace = 20
+status34 = 45.6
+Criminal = True
+text = "Competitive Programming"
+person = {"name": "Leul", "age": 31}
+
+
+print(type(person))
+
+
+# print(type(text))
+# print(type(Criminal))
+# print(type(status34))
+# print(type(class_attendace))
+
+
--- test.py
+++ test.py
@@ -3,6 +3,15 @@ grade = 3
 
 # this code is commented
 
+"""
+ this tha
+ tere here later 
+"""
+
+variable = 8
+
+Variable = 9
+
 if age >= 18:
     if grade >= 2.5:         
        print("You are old")
