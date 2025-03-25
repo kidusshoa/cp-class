8825ec06a9d23f89b3bcd423cb141816a3e8e033
kidusshoa <captainkevin2008@gmail.com>
2025-03-25 22:31:00 +0300

    lists

---
+++ list/index.py
@@ -0,0 +1,20 @@
+name_list = ["kidus", "betty", "nati"]
+
+# print(name_list[0])
+# print(len(name_list))
+
+# name_list2 = name_list.sort()
+# print(name_list2)
+
+# nums = [4, 5, 11, 2, 6 , 8]
+# nums.sort()
+# print(nums[0])
+# nums.sort()
+# nums.reverse()
+
+# for num in nums:
+# 	print(num)
+
+# nums = [1, 2, 3, 4, 5, 6]
+# even_sum = sum([x for x in nums if x % 2 == 0])
+# print(even_sum)
