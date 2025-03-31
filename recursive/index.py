d07e3814a50223f3c01f605f791ff96f4e562bf6
kidusshoa <captainkevin2008@gmail.com>
2025-03-31 22:50:46 +0300

    recursive

---
+++ recursive/index.py
@@ -0,0 +1,35 @@
+# def factorial(n):
+#     # Base case
+#     if n == 0 or n == 1:
+#         return 1
+#     # Recursive case
+#     else:
+#         return n * factorial(n - 1)
+
+# print(factorial(4))
+
+# 1, 1, 2, 3, 5, 8, 13, 21
+
+# def fibonacci_recursive(n):
+#     if n == 0:
+#         return []
+#     elif n == 1:
+#         return [0]
+#     elif n == 2:
+#         return [0, 1]
+#     else:
+#         sequence = fibonacci_recursive(n - 1)
+#         sequence.append(sequence[-1] + sequence[n-2])
+#         return sequence
+
+# print(fibonacci_recursive(6))
+
+# def fib(n):
+#     if n <= 1:
+#         return n
+#     while(n>=1):
+#         n = n -1
+#         print( ( fib(n-1) + fib(n-2)))
+    
+    
+# print(fib(6))      
